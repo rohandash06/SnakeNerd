e38f3728ec5d1329fd0b42b6838d00062bd87218
rohandash06 <37982545+rohandash06@users.noreply.github.com>
2019-07-29 12:21:03 -0400

    Add files via upload

---
+++ server/onstartup.js
@@ -0,0 +1,7 @@
+// Meteor.startup(function () {
+// 	console.log(process.env);
+// 	console.log(process.env.MONGO_URL);
+// 	process.env.MONGO_URL = "mongodb://127.0.0.1:3001/meteor";
+// 	console.log(process.env.MONGO_URL);
+// 	console.log(process.env);
+// });
